009661827936b644087912dbcbc4ceddcd8c836f
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-07 19:16:25 +0100

    feat: Added nonce validation

--- src/main/python/nonce.py
+++ src/main/python/nonce.py
@@ -1,7 +1,54 @@
+import json
+import uuid
+
+import json
 import uuid
 
 
 class NonceManager:
-    def generate_nonce(self):
-        nonce = str(uuid.uuid4())
-        return nonce
+    """
+    NonceManager: Manage nonces to prevent replay attacks.
+    """
+
+    def __init__(self, path_old_nonces):
+        """
+        Initialize NonceManager.
+
+        Args:
+            path_old_nonces (str): Path to the file storing old nonces.
+        """
+        self.path_old_nonces = path_old_nonces
+
+    def generate(self):
+        """
+        Generate a new nonce.
+
+        Returns:
+            str: Newly generated nonce.
+        """
+        return str(uuid.uuid4())
+
+    def not_repeated(self, nonce):
+        """
+        Check if the nonce is not repeated and update the file accordingly.
+
+        Args:
+            nonce (str): Nonce to check.
+
+        Returns:
+            bool: True if the nonce is not repeated, False otherwise.
+        """
+        try:
+            with open(self.path_old_nonces, "r") as file:
+                old_nonces = json.load(file)
+        except FileNotFoundError:
+            old_nonces = []
+
+        if nonce in old_nonces:
+            return False
+        else:
+            old_nonces.append(nonce)
+            with open(self.path_old_nonces, "w") as file:
+                json.dump(old_nonces, file)
+            return True
+
