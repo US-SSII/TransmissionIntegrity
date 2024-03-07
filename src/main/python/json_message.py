26e9c02ad13106d65a716d536b45b38078009dee
Ignarrman <nacho.arromant@gmail.com>
2024-03-07 19:10:19 +0100

    jonson: Añade creación de Mensajes y objeto Mensaje

--- src/main/python/json_message.py
+++ src/main/python/json_message.py
@@ -1,2 +1,23 @@
+import json
+
+
 class JSONMessage:
-    pass
+    def __init__(self, origin_account, reciever_account, amount):
+        self.origin_account = origin_account
+        self.reciever_account = reciever_account
+        self.amount = amount
+
+    def to_dict(self):
+        return {'origin_account': self.origin_account, 'reciever_account': self.reciever_account, 'amount': self.amount}
+
+def create_message():
+    origin_account = input("Origen: ")
+    reciever_account = input("Destino: ")
+    amount = input("Cantidad: ")
+
+    message = JSONMessage(origin_account, reciever_account, amount)
+    message_dict = message.to_dict()
+    message_json = json.dumps(message_dict, ensure_ascii=False).encode('utf-8')
+
+    return message_json
+
