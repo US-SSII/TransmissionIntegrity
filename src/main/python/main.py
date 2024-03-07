e62f1bb022e365bccaba508d53eac17b5a4daa60
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-03-07 20:13:53 +0100

    feat: Added communication

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,10 +1,37 @@
 import threading
 from configparser import ConfigParser
+from datetime import time
 
 from server import Server
+from src.main.python.client import Client
 from src.main.python.json_message import create_message
 
 if __name__ == '__main__':
-    create_message()
+    config = ConfigParser()
+    config.read("config.ini")
+
+    # SERVER
+    host = config.get("SERVER", "host")
+    port = config.get("SERVER", "port")
+
+    server = Server(host, int(port))
+    server_thread = threading.Thread(target=server.start)
+    server_thread.start()
+
+    client = Client(host, int(port))
+    client.connect()
+
+    try:
+        while True:
+            message = create_message()
+            client.send_message(message)
+            response = client.receive_message()
+            print(response)
+    except KeyboardInterrupt:
+        client.close()
+
+    server_thread.join()
+
+
 
 
